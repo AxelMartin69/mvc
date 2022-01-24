dcfffd46786798e79eee2affc97af4ad05ca17ef
AxelMartin69 <75273577+AxelMartin69@users.noreply.github.com>
2022-01-24 11:34:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 class Model {
-	constructor() {}
+	constructor() {
+		// data
+		this.tasks = [
+			{id:1, text: 'Be good', complete: false},
+			{id:2, text: 'Be nice', complete: false}
+		]
+	}
 }
 
 class View {
-	constructor() {}
+	constructor() {
+		// basic view
+		// root element
+		this.app = this.getElement('h1')
+		// title
+		this.title = this.setElement('h1')
+		this.title = textContent = 'Tasks'
+		// task list
+		this.taskList = this.setElement('ul')
+		// append title and task list to app
+		this.app.append(this.title, this.taskList)
+		
+	}
+
+	getElement(selector){
+		const element = document.querySelector(selector)
+		return element
+	}
+
+	setElement(tag, classname){
+		const element = document.querySelector(tag)
+		if(classname !== undefined){
+			element.classList.add(classname)
+		}
+		return element
+	}
 }
 
 class Controller {
